5778f077b892e58e255511df2cb252d5953c48ed
y0ub <y0lit7515@gmail.com>
2024-03-30 15:26:23 +0900

    update: size of default enemy

--- src/enemy.cc
+++ src/enemy.cc
@@ -3,7 +3,7 @@
 
 enemy::enemy(const float &kheight, const float &kwidth)
 {
-  radius_ = (2.0f / std::min(kwidth, kheight) * 50.0f);
+  radius_ = (2.0f / 50.0f);
   position_.x = 0;
   position_.y = 0.5f;
   circle_object_ = std::move(std::make_unique<circle>(position_.x, position_.y, radius_));
