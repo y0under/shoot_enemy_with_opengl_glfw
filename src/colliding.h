f633a79e72352f9861568a75818e6f190392d9b5
y0ub <y0lit7515@gmail.com>
2024-04-01 21:13:24 +0900

    add: colliding.

---
+++ src/colliding.h
@@ -0,0 +1,11 @@
+#ifndef GLFW_PRAC_SRC_COLLIDING
+#define GLFW_PRAC_SRC_COLLIDING
+
+#include "circle.h"
+
+class colliding {
+  public:
+    bool is_colliding(const circle &a, const circle &b);
+};
+
+#endif
